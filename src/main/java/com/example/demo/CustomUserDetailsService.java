c5311a03440a89fb24df1680eb7db52252ceab08
Renan <renaner123@gmail.com>
2022-04-10 16:59:28 -0300

    Verify user by email on database

---
+++ src/main/java/com/example/demo/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	//buscar um usuário pelo email no banco de dados
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		
+		User user = repo.findByEmail(email);
+		if(user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		
+		return new CustomUserDetails(user);
+	}
+
+}
